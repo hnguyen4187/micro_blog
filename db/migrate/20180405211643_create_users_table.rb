39806b671953c6ef7873bba473c44f710d726244
Dorlorian Dickson <ddickson634@gmail.com>
2018-04-10 12:25:50 -0400

    feed.erb, CUT.rb, MBSQlite3 pushed

--- db/migrate/20180405211643_create_users_table.rb
+++ db/migrate/20180405211643_create_users_table.rb
@@ -1,9 +1,9 @@
 class CreateUsersTable < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-          t.string :fname
-          t.string :lname
-          t.string :password
+        t.string :fname
+        t.string :lname
+        t.string :password
       end
   end
 end
