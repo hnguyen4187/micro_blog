bb7a56c759a33c62e2fd3799792b44904107805e
Dorlorian Dickson <ddickson634@gmail.com>
2018-04-13 10:12:09 -0400

    made a few changes to the file order.

--- app.rb
+++ app.rb
@@ -4,36 +4,15 @@ require 'bundler/setup'
 require './models'
 set :database, "sqlite3:micro_blog.sqlite3"
 
+def current_user
+    if session[:user_id]
+        @current_user = User.find(session[:user_id])
+    end
+end
+
 get '/' do
     #contains log in and signup options.
   erb :home
-end
-
-post '/single_user/new' do
-  puts params
-    @new_user = User.create(params[:new_user])
-    redirect "/single_user/#{@new_user.id}"
-
-end
-
-get '/all_users' do
-  @users = User.all
-    erb :all_users
-end
-
-get '/single_user/:id' do
-    @user = User.find(params[:id])
-    @posts = @user.posts
-    erb :single_user
-end
-
-get '/post' do
-    erb :post
-end
-
-get '/post/:id' do
-    @post = Post.find(params[:id])
-    erb :post
 end
 
 post '/sign-in' do
@@ -51,12 +30,48 @@ get '/log_out' do
     redirect "/"
 end
 
+post '/single_user/new' do
+  puts params
+    @new_user = User.create(params[:new_user])
+    redirect "/single_user/#{@new_user.id}"
+
+end
+
+
+get '/single_user/:id' do
+    @user = User.find(params[:id])
+    session[:user_id] = @user.id
+    @posts = @user.posts
+    erb :single_user
+end
+
+post '/single_user/edit/:id' do
+    @update = params[:user]
+    @user = User.find(params[:id])
+    @user.update(@update)
+    redirect "/single_user/#{@user.id}"
+end
+
 get '/single_user/delete/:id' do
     @user = User.find(params[:id])
     @user.destroy
     session.clear
     redirect "/"
     end
+
+get '/all_users' do
+  @users = User.all
+    erb :all_users
+end
+
+get '/post' do
+    erb :post
+end
+
+get '/post/:id' do
+    @post = Post.find(params[:id])
+    erb :post
+end
 
 post '/post/new' do
     @new_post = Post.create(params[:new_post])
